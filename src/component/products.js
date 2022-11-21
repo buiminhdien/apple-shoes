978cd857760858fd5a392aaf113f9cf81b9ef9ef
viet <viet52667@donga.edu.vn>
2022-11-21 18:58:07 +0700

    finsh

---
+++ src/component/products.js
@@ -0,0 +1,39 @@
+const products = [
+    {
+        category: "Điện thoại Smartphone",
+        cover_image: "https://salt.tikicdn.com/cache/280x280/ts/product/e4/b1/a3/17c87d7352a5c06a301569802f1f0140.jpg",
+        price: "22.990.000 ₫",
+        stocked: true, 
+        name: "Điện thoại Samsung Galaxy S22 Ultra 5G (8GB/128GB) - Hàng Chính Hãng"
+    },
+    {
+        category: "Điện thoại Smartphone",
+        cover_image: "https://salt.tikicdn.com/cache/200x200/ts/product/b5/d2/0f/c97d2695f7658b4b26b2a3ff3d61a6e2.jpg",
+        price: "30.200.000 ₫",
+        stocked: true, 
+        name: "Điện Thoại iPhone 13 Pro Max 128GB - Hàng Chính Hãng"
+    },
+    {
+        category: "Thiết Bị Số",
+        cover_image: "https://salt.tikicdn.com/cache/280x280/ts/product/57/10/d8/1ce4259d9fe8c0200da3240f0973176a.jpg",
+        price: "11.990.000 đ",
+        stocked: false, 
+        name: "Đồng hồ thông minh Samsung Galaxy Watch 5 Pro Bluetooth (45mm) R920 - Hàng Chính Hãng"
+    },
+    {
+        category: "Thiết Bị Số",
+        cover_image: "https://salt.tikicdn.com/cache/200x200/ts/product/e8/82/08/e8f1f66434acb88abe32ffa9d6bcb213.jpg",
+        price: "2.850.000 ₫",
+        stocked: true, 
+        name: "Combo mic thu âm Takstar PC K600, Soundcard K10 - HÀNG CHÍNH HÃNG"
+    },
+    {
+        category: "Thiết bị gia đình",
+        cover_image: "https://salt.tikicdn.com/cache/280x280/ts/product/13/af/19/a9ff9d5460a27403964002818d975feb.jpg",
+        price: "10.534.000 ₫",
+        stocked: true, 
+        name: "Máy giặt Toshiba 9.5 kg TW-BL105A4V(SS) -Hàng chính hãng-Giao toàn quốc"
+    },
+];
+
+export default products;
